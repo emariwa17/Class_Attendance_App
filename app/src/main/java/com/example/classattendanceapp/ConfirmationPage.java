fd647e4f2895fd239db06c0af765678ab2f41aa5
emariwa11 <emariwa17@alustudent.com>
2020-02-21 00:58:24 +0300

    Commit repo

--- app/src/main/java/com/example/classattendanceapp/ConfirmationPage.java
+++ app/src/main/java/com/example/classattendanceapp/ConfirmationPage.java
@@ -3,6 +3,7 @@ package com.example.classattendanceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ConfirmationPage extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class ConfirmationPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation_page);
     }
+
+
 }
