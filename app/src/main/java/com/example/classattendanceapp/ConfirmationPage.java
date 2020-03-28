a18d4407fd72d3b32a3af604a0b63ad81e5f8bb2
emariwa11 <emariwa17@alustudent.com>
2020-03-28 20:14:35 +0300

    NFC Implementation

--- app/src/main/java/com/example/classattendanceapp/ConfirmationPage.java
+++ app/src/main/java/com/example/classattendanceapp/ConfirmationPage.java
@@ -2,6 +2,7 @@ package com.example.classattendanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,4 +14,8 @@ public class ConfirmationPage extends AppCompatActivity {
         setContentView(R.layout.activity_confirmation_page);
     }
 
+    public void addRecord(View view) {
+        Intent myActivity = new Intent(this, ConfirmationPage.class);
+        startActivity(myActivity);
+    }
 }
